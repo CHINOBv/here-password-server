31477c0058b982e7f0b14afe981e0938c6a395ec
chinobv <retrochanel97@gmail.com>
2020-07-11 20:09:22 -0500

    Frist commit

---
+++ src/models/users.model.ts
@@ -0,0 +1,47 @@
+import { model, Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  name: {
+    unique: true,
+    required: true,
+    type: String,
+  },
+  user: String,
+  password: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+});
+
+export interface IUser extends Document {
+  name: string;
+  user: string;
+  password: string;
+  email: string;
+}
+
+userSchema.pre<IUser>("save", async function (next) {
+  const user = this;
+  if (
+    !user.isModified("password") ||
+    !user.isModified("email") ||
+    !user.isModified("user")
+  )
+    return next();
+  const salt = await bcrypt.genSalt(12);
+  const passHash = await bcrypt.hash(user.password, salt);
+  const emailHash = await bcrypt.hash(user.email, salt);
+  user.password = await passHash;
+  user.email = await emailHash;
+  next();
+});
+
+export default model<IUser>("User", userSchema);
