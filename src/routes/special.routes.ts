a44545aeeaaf6eda4b89cec3f780fb4ab899b4ba
chinobv <retrochanel97@gmail.com>
2020-07-14 18:01:33 -0500

    Get account by id and user id

--- src/routes/special.routes.ts
+++ src/routes/special.routes.ts
@@ -40,4 +40,28 @@ router.get(
   }
 );
 
+router.get(
+  "/account",
+  passport.authenticate("jwt", { session: false }),
+  async (req: Request, res: Response): Promise<Response> => {
+    if (!req.body.accountid) {
+      return res.status(400).json({ msg: "All fields are Required" });
+    }
+    const user = await User.findById({ _id: req.body.userid });
+    if (!user) {
+      return res.status(400).json({ msg: "User Invalid" });
+    }
+
+    const account = await Account.findOne({
+      _id: req.body.accountid,
+      userid: user.id,
+    });
+    if (!account) {
+      return res.status(400).json({ msg: "Account not Found" });
+    }
+
+    return res.send(account);
+  }
+);
+
 export default router;
