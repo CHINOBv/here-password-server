fa5b8786ce9131701d3e180ea25e833ff7a61081
chinobv <retrochanel97@gmail.com>
2020-07-14 18:28:29 -0500

    Add method for get password of account with userid, password of user and account id

--- src/routes/special.routes.ts
+++ src/routes/special.routes.ts
@@ -1,11 +1,15 @@
-import express, { Router, Request, Response } from "express";
+import { Router, Request, Response } from "express";
 import passport, { session } from "passport";
-const app = express();
-const router = Router();
+import CryptoJS from "crypto-js";
+import dotenv from "dotenv";
+dotenv.config();
 
 import Account, { IAccount } from "../models/accounts.model";
-import User from "../models/users.model";
+import User, { IUser } from "../models/users.model";
 
+const secretCrypt = process.env.ACCOUNT_SECRET;
+
+const router = Router();
 router.post(
   "/accounts",
   passport.authenticate("jwt", { session: false }),
@@ -61,6 +65,44 @@ router.get(
     }
 
     return res.send(account);
+  }
+);
+
+router.get(
+  "/account/password",
+  passport.authenticate("jwt", { session: false }),
+  async (req: Request, res: Response): Promise<Response> => {
+    if (!req.body.user || !req.body.password) {
+      return res.status(400).json({ msg: "All Fields are Required" });
+    }
+
+    const user = await User.findOne({ user: req.body.user });
+    if (!user) {
+      return res.status(400).json({ msg: "This User no Exist" });
+    }
+
+    const isMatch = await user.comparePassword(req.body.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "User or Passwor are Incorrect" });
+    }
+    const account = await Account.findOne({
+      userid: user._id,
+      _id: req.body.accountid,
+    });
+    if (!account) {
+      return res.status(400).json({ msg: "Account not Find" });
+    }
+    const decrypt = await CryptoJS.AES.decrypt(
+      account.password,
+      `${secretCrypt}`
+    );
+    if (!decrypt) {
+      return res.status(500).json({ msg: "Error to get password" });
+    }
+
+    const pass = await decrypt.toString(CryptoJS.enc.Utf8);
+
+    return res.status(200).send(pass);
   }
 );
 
